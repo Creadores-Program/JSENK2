cd80d7435aa71f3cacb42571be93b5d0341bd470
Trollhunters501PC <cpcreador@gmail.com>
2024-11-14 22:22:24 -0600

    aña

--- src/org/CreadoresProgram/JSENK2/index.js
+++ src/org/CreadoresProgram/JSENK2/index.js
@@ -52,14 +52,60 @@
   let PluginsPathDir = new java.io.File(PathDir + "JSENK2plugins/");
   PluginsPathDir.mkdir();
   console.info("§eLoading Plugins JSENK2...");
+  let pluginsJSENK2 = [];
   for each(let plJSE in java.util.Objects.requireNonNull(PluginsPathDir.listFiles())){
     if(plJSE.isDirectory() || !plJSE.getName().endsWith(".tgz")) continue;
     let DirPL = readTgz(plJSE);
     let packManiPL = JSON.parse(new java.lang.String(DirPL["package/package.json"]));
-    execJsModern(new java.lang.String(DirPL["package/" + packManiPL.main]), packManiPL.name+"/"+packManiPL.main);
+    if(pluginsJSENK2.indexOf(packManiPL.name) != -1){
+      console.error("§cPlugin "+packManiPL.name+" Duplicate, remove one");
+      continue;
+    }
+    pluginsJSENK2[pluginsJSENK2.length] = packManiPL.name;
+    console.info("§eLoading Plugin "+ packManiPL.name+"...");
+    for each(let filePLT in Object.keys(DirPL)){
+      if(filePLT.endsWith(".js")){
+        execJsModern(new java.lang.String(DirPL[filePLT]), "pluginsJSENK2/"+packManiPL.name+filePLT.replace("package", ""));
+      }
+      if(filePLT.endsWith(".json")){
+        execJsModern("module.exports = "+ new java.lang.String(DirPL[filePLT]) + ";", "pluginsJSENK2/"+packManiPL.name+filePLT.replace("package", ""));
+      }
+    }
   }
+  console.info("§eLoading Scripts...");
   for each(let script2s in java.util.Objects.requireNonNull(FilePathDir.listFiles())){
     if((script2s.isDirectory() || !script2s.getName().endsWith(".tgz")) || script2s.getAbsolutePath().startsWith(PluginsPathDir.getAbsolutePath())) continue;
     let DirScript = readTgz(script2s);
+    let packManiSc = JSON.parse(new java.lang.String(DirPL["package/package.json"]));
+    if(script.getScriptByName(packManiSc.name) != null){
+      console.error("§cScript "+packManiSc.name+" Duplicate, remove one");
+      continue;
+    }
+    console.info("§eLoading "+packManiSc.name+"...");
+    for each(let fileScr in Object.keys(DirScript)){
+      if(fileScr.endsWith(".js")){
+        execJsModern(new java.lang.String(DirScript[fileScr]), fileScr.replace("package", packManiSc.name));
+      }
+      if(fileScr.endsWith(".ts")){
+        if(pluginsJSENK2.indexOf("typescript") != -1){
+          let typesc = contexto2eng.require("pluginsJSENK2/typescript/typescript.js");
+          execJsModern(typesc.transpile(new java.lang.String(DirScript[fileScr])), fileScr.replace("package", packManiSc.name));
+        }
+      }
+      if(fileScr.endsWith(".json")){
+        execJsModern("module.exports = "+ new java.lang.String(DirScript[fileScr]) +";", fileScr.replace("package", packManiSc.name));
+      }
+    }
+    script.registerScript({
+      name: packManiSc.name,
+      version: packManiSc.version,
+      description: packManiSc.description,
+      website: packManiSc.homepage,
+      author: packManiSc.author
+    });
+    let mainSc = contexto2eng.require(packManiSc.name+"/"+packManiSc.main);
+    script.addEventListener("Enable", (mainSc.onEnable || new Function()));
+    script.addEventListener("Load", (mainSc.onLoad || new Function()));
+    script.addEventListener("Disable", (mainSc.onDisable || new Function()));
   }
 })();
